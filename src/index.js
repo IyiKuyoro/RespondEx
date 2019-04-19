df70cdb0f6d9475aeff2c312a6ffb4ff58d9df31
IyiKuyoro <opeoluwa.iyikuyoro@gmail.com>
2019-04-19 12:28:52 +0100

    feat(resource-creation): add method for creation response
    - modiy gitignore
    - add new dev dependencies
    - add the method to respond to resource creation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 export default class RespondEx {
-  static createResourceSuccess() {
-    return 'Hello World';
+  /**
+   * @description Send a success response for a created resource.
+   * @param  {string} message The message of the response
+   * @param  {object} data The data to be sent
+   * @param  {string} location The URL of the new resource
+   * @param  {object} res The express response object
+   * @param  {object} options Object containing options.
+   */
+  static createdResource(message, data, location, res, options = {}) {
+    res.set({
+      'content-type': options.contentType || 'application/json',
+      url: location,
+    });
+    res.status(200).json({
+      success: true,
+      message,
+      data,
+    });
   }
 }
