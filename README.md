aa0ea12aff12272764d2ac4c3b63e74dacf10166
IyiKuyoro <opeoluwa.iyikuyoro@gmail.com>
2019-04-19 18:33:55 +0100

    feat(errors): add handler for some client errors
    - add handler for errors
    - add handler for errors by type
    - update documentation

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# RespondEx [![Build Status](https://travis-ci.org/IyiKuyoro/RespondEx.svg?branch=develop)](https://travis-ci.org/IyiKuyoro/RespondEx) [![Coverage Status](https://coveralls.io/repos/github/IyiKuyoro/RespondEx/badge.svg?branch=develop)](https://coveralls.io/github/IyiKuyoro/RespondEx?branch=develop) [![Maintainability](https://api.codeclimate.com/v1/badges/aefd9dedd80b68dd6212/maintainability)](https://codeclimate.com/github/IyiKuyoro/RespondEx/maintainability) [![npm version](https://badge.fury.io/js/respondex.svg)](https://badge.fury.io/js/respondex)
+# @respondex/core [![Build Status](https://travis-ci.org/IyiKuyoro/RespondEx.svg?branch=develop)](https://travis-ci.org/IyiKuyoro/RespondEx) [![Coverage Status](https://coveralls.io/repos/github/IyiKuyoro/RespondEx/badge.svg?branch=develop)](https://coveralls.io/github/IyiKuyoro/RespondEx?branch=develop) [![Maintainability](https://api.codeclimate.com/v1/badges/aefd9dedd80b68dd6212/maintainability)](https://codeclimate.com/github/IyiKuyoro/RespondEx/maintainability)
 
 This is a simple node package that helps developers package HTTP responses into simple reusable methods, without having to worry about setting the headers and the right status codes.
 
@@ -7,16 +7,16 @@ All you need to do is call the right method with all the information required al
 [Express](https://expressjs.com/) is a powerful framework that helps handle HTTP requests and responses on the server. However, it can become a bit repetitive to have to respond to various request the same way. This would help take all that repetitive "res" code and stow it away in the node_modules. 😄
 
 ## Getting Started
-Your REST API must be using [express](https://expressjs.com/) as its HTTP http handler.
-- Download the package as a dev dependency with 
+Your REST API must be using [express](https://expressjs.com/) as its HTTP handler.
+- Download the package as a development dependency with 
 ```
-npm install respondex
+npm install @respondex/core @respondex/apierror
 ```
 - Import the package into your controller, middleware or wherever you wish to send a response from.
 ```
-import RespondEx from 'respondex'
+import RespondEx from '@respondex/core'
 ```
-- Use by simply send a response as demonstrated below for a newly created product resource.
+- Use by simply sending a response as demonstrated below for a newly created product resource.
 ```
 RespondEx.createdResource(
   'Successful sign-in',
@@ -34,13 +34,64 @@ RespondEx.createdResource(
 ### Methods
 **createdResource**: Sends a HTTP response for a newly created resource. Response will contain a 201 status code, message and data in the body. It would also contain the URL to the newly created resource in the header of the response.
 
-| Parameters | Type   | Description                              | Example                                       |
-|------------|--------|------------------------------------------|-----------------------------------------------|
-| message    | string | The message to be sent with the response | "Product successfully created."               |
-| data       | object | Newly created resource data              | { name: "Chair" }                             |
-| location   | string | The URL of the newly created resource    | "https://fakeecommerce.com/api/v1/products/1" |
-| res        | object | The express http response object         |                                               |
-| options    | object | Some extra headers                       | { contentType: "application/json" }           |
+| Parameters             | Type   | Description                              | Example                                       |
+|------------------------|--------|------------------------------------------|-----------------------------------------------|
+| message                | string | The message to be sent with the response | "Product successfully created."               |
+| data                   | object | Newly created resource data              | { name: "Chair" }                             |
+| location               | string | The URL of the newly created resource    | "https://fakeecommerce.com/api/v1/products/1" |
+| res                    | object | The express http response object         |                                               |
+| options (_optional_)   | object | Some extra headers                       | { contentType: "application/json" }           |
+
+#### Example
+```
+RespondEx.createdResource(
+  'Successful sign-in',
+  {
+    name: 'Chair',
+    quantity: 100,
+    description: 'A comfortable seat for your afternoon tea.'
+  },
+  'https://fakeecommerce.com/api/v1/products/1',
+  res,
+);
+```
+
+**error**: Sends a HTTP response error using an instance of the APIError class.
+
+| Parameter             | Type     | Description                              | Example                                       |
+|------------------------|----------|------------------------------------------|-----------------------------------------------|
+| error                  | APIError | An instance of the APIError object       |                                               |
+| res                    | object   | The express http response object         |                                               |
+| options (_optional_)   | object   | Some extra headers                       | { contentType: "application/json" }           |
+
+#### Example
+```
+import APIError from "@respondex/apierror"
+
+const error = new APIError(...);
+RespondEx.error(
+  error,
+  res,
+);
+```
+
+**errorByType**: Sends a HTTP response error by providing an error type.
+
+| Parameter                | Type     | Description                              | Example                                       |
+|---------------------------|----------|------------------------------------------|-----------------------------------------------|
+| type                      | string   | The type of error to be sent             | NotFound                                      |
+| res                       | object   | The express http response object         |                                               |
+| message (_optional_)      | object   | The message to be sent in the body       | "Some error has occurred"                     |
+| possibleErrors (_optional_)| object[]| The express http response object         |                                               |
+| options (_optional_)      | object   | Some extra headers                       | { contentType: "application/json" }           |
+
+#### Example
+```
+RespondEx.errorByType(
+  'NotFound',
+  res,
+);
+```
 
 ### Options
 This is an optional parameter that can be provided to the methods to configure some of the information in the header. Currently there is just one.
@@ -48,6 +99,17 @@ This is an optional parameter that can be provided to the methods to configure s
 | Property    | Type   | Example            |
 |-------------|--------|--------------------|
 | contentType | string | "application/json" |
+
+### Errors
+This is the list of error types
+
+| Error        | code | Description                     |
+|--------------|------|---------------------------------|
+| ClientError  | 400  | For general client errors       |
+| Unauth­orized | 401  | For unauthorized access errors  |
+| Forbidden    | 403  | For for forbidden access errors |
+| NotFound     | 404  | Resource not found error        |
+| _default_    | 500  | For general server errors       |
 
 ### Contributors
 _Opeoluwa Iyi-Kuyoro_: 👨🏿[Profile](https://github.com/IyiKuyoro) - [WebSite](https://iyikuyoro.com)
--- src/__tests__/RespondEx.spec.js
+++ src/__tests__/RespondEx.spec.js
@@ -1,4 +1,5 @@
 import RespondEx from '..';
+import ApiError from '@respondex/apierror';
 import ResMock from '../__mocks__/ResMock';
 
 describe('RespondEx', () => {
@@ -58,6 +59,348 @@ describe('RespondEx', () => {
           number: 100,
           description: 'A comfortable chair for your afternoon tea',
         },
+      });
+    });
+  });
+
+  describe('error', () => {
+    let res;
+    let statusSpy;
+    let jsonSpy;
+    let setSpy;
+
+    beforeEach(() => {
+      res = new ResMock();
+      statusSpy = jest.spyOn(res, 'status');
+      jsonSpy = jest.spyOn(res, 'json');
+      setSpy = jest.spyOn(res, 'set');
+    });
+
+    afterEach(() => {
+      jest.resetAllMocks();
+    });
+
+    it('should set the response type to application/json', () => {
+      const error = new ApiError('Some error', [], 400);
+
+      RespondEx.error(error, res);
+
+      expect(setSpy).toHaveBeenCalledWith({
+        'content-type': 'application/json',
+      });
+    });
+
+    it('should set the status code to 400', () => {
+      const error = new ApiError('Some error', ['error'], 400);
+
+      RespondEx.error(error, res);
+
+      expect(statusSpy).toHaveBeenCalledWith(400);
+      expect(jsonSpy).toHaveBeenCalledWith({
+        success: false,
+        message: 'Some error',
+        possibleCauses: ['error'],
+      });
+    });
+
+    it('should set the status code to 500 with custom possible cases', () => {
+      const error = new ApiError('Some error');
+
+      RespondEx.error(error, res);
+
+      expect(statusSpy).toHaveBeenCalledWith(500);
+      expect(jsonSpy).toHaveBeenCalledWith({
+        success: false,
+        message: 'Some error',
+        possibleCauses: [],
+      });
+    });
+  });
+
+  describe('errorByType', () => {
+    describe('ClientError', () => {
+      let res;
+      let respondexSpy;
+
+      beforeEach(() => {
+        res = new ResMock();
+        respondexSpy = jest.spyOn(RespondEx, 'error')
+          .mockImplementation(() => {});
+      });
+
+      afterEach(() => {
+        jest.resetAllMocks();
+      });
+
+      it('should call RespondEx.error with default message', () => {
+        RespondEx.errorByType(
+          'ClientError',
+          res,
+        );
+
+        const error = new ApiError(
+          'A client error has occurred.',
+          [
+            'You may not be sending all the information required',
+            'Some of the information may be in the wrong type or format',
+          ],
+          400,
+        );
+
+        expect(respondexSpy).toHaveBeenCalledWith(
+          error,
+          res,
+          {},
+        );
+      });
+
+      it('should call RespondEx.error with custom message', () => {
+        RespondEx.errorByType(
+          'ClientError',
+          res,
+          'A custom message',
+          [],
+        );
+
+        const error = new ApiError(
+          'A custom message',
+          [],
+          400,
+        );
+
+        expect(respondexSpy).toHaveBeenCalledWith(
+          error,
+          res,
+          {},
+        );
+      });
+    });
+
+    describe('Unauthorized', () => {
+      let res;
+      let respondexSpy;
+
+      beforeEach(() => {
+        res = new ResMock();
+        respondexSpy = jest.spyOn(RespondEx, 'error')
+          .mockImplementation(() => {});
+      });
+
+      afterEach(() => {
+        jest.resetAllMocks();
+      });
+
+      it('should call RespondEx.error with default message', () => {
+        RespondEx.errorByType(
+          'Unauth­orized',
+          res,
+        );
+
+        const error = new ApiError(
+          'Unauthorized',
+          [
+            'Authorization for this resource failed.',
+          ],
+          401,
+        );
+
+        expect(respondexSpy).toHaveBeenCalledWith(
+          error,
+          res,
+          {},
+        );
+      });
+
+      it('should call RespondEx.error with custom message', () => {
+        RespondEx.errorByType(
+          'Unauthorized',
+          res,
+          'A custom message',
+          [],
+        );
+
+        const error = new ApiError(
+          'A custom message',
+          [],
+          401,
+        );
+
+        expect(respondexSpy).toHaveBeenCalledWith(
+          error,
+          res,
+          {},
+        );
+      });
+    });
+
+    describe('Forbidden', () => {
+      let res;
+      let respondexSpy;
+
+      beforeEach(() => {
+        res = new ResMock();
+        respondexSpy = jest.spyOn(RespondEx, 'error')
+          .mockImplementation(() => {});
+      });
+
+      afterEach(() => {
+        jest.resetAllMocks();
+      });
+
+      it('should call RespondEx.error with default message', () => {
+        RespondEx.errorByType(
+          'Forbidden',
+          res,
+        );
+
+        const error = new ApiError(
+          'Access denied',
+          [
+            'You may not have the right access level to this resource',
+          ],
+          403,
+        );
+
+        expect(respondexSpy).toHaveBeenCalledWith(
+          error,
+          res,
+          {},
+        );
+      });
+
+      it('should call RespondEx.error with custom message', () => {
+        RespondEx.errorByType(
+          'Forbidden',
+          res,
+          'A custom message',
+          [],
+        );
+
+        const error = new ApiError(
+          'A custom message',
+          [],
+          403,
+        );
+
+        expect(respondexSpy).toHaveBeenCalledWith(
+          error,
+          res,
+          {},
+        );
+      });
+    });
+
+    describe('NotFound', () => {
+      let res;
+      let respondexSpy;
+
+      beforeEach(() => {
+        res = new ResMock();
+        respondexSpy = jest.spyOn(RespondEx, 'error')
+          .mockImplementation(() => {});
+      });
+
+      afterEach(() => {
+        jest.resetAllMocks();
+      });
+
+      it('should call RespondEx.error with default message', () => {
+        RespondEx.errorByType(
+          'NotFound',
+          res,
+        );
+
+        const error = new ApiError(
+          'Resource not found',
+          [
+            'The URL may be wrong',
+          ],
+          404,
+        );
+
+        expect(respondexSpy).toHaveBeenCalledWith(
+          error,
+          res,
+          {},
+        );
+      });
+
+      it('should call RespondEx.error with custom message', () => {
+        RespondEx.errorByType(
+          'NotFound',
+          res,
+          'A custom message',
+          [],
+        );
+
+        const error = new ApiError(
+          'A custom message',
+          [],
+          404,
+        );
+
+        expect(respondexSpy).toHaveBeenCalledWith(
+          error,
+          res,
+          {},
+        );
+      });
+    });
+
+    describe('ServerError', () => {
+      let res;
+      let respondexSpy;
+
+      beforeEach(() => {
+        res = new ResMock();
+        respondexSpy = jest.spyOn(RespondEx, 'error')
+          .mockImplementation(() => {});
+      });
+
+      afterEach(() => {
+        jest.resetAllMocks();
+      });
+
+      it('should call RespondEx.error with default message', () => {
+        RespondEx.errorByType(
+          'ServerError',
+          res,
+        );
+
+        const error = new ApiError(
+          'A server error has occurred! Please contact the administrator',
+          [
+            'This error is caused by a malfunction in this application',
+          ],
+          404,
+        );
+
+        expect(respondexSpy).toHaveBeenCalledWith(
+          error,
+          res,
+          {},
+        );
+      });
+
+      it('should call RespondEx.error with custom message', () => {
+        RespondEx.errorByType(
+          'ServerError',
+          res,
+          'A custom message',
+          [],
+        );
+
+        const error = new ApiError(
+          'A custom message',
+          [],
+          404,
+        );
+
+        expect(respondexSpy).toHaveBeenCalledWith(
+          error,
+          res,
+          {},
+        );
       });
     });
   });
